f4032249ad03c7aee246a2566f2aa3558a5a638b
Neel Sharma <100556796+Kingslayer1000@users.noreply.github.com>
2023-06-22 15:20:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1=Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage="dice" + randomNumber1 + ".png";
+
+var randomImageSource="images/"+randomDiceImage;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2=Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+
+document.querySelector(".img2").setAttribute("src",randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+document.querySelector("h1").innerText="1️⃣Player One Wins!";
+}
+
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerText="Player Two Wins2️⃣!";
+}
+
+else{
+    document.querySelector("h1").innerText="Its a draw!";  
+}
